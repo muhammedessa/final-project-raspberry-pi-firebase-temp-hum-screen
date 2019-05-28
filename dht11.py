dd8b03a78d5ab33cb478d9f5cdeec445d2484313
Muhammed Essa <muhammed.essa@gmail.com>
2019-05-28 03:00:42 -0700

    done

---
+++ dht11.py
@@ -0,0 +1,10 @@
+import sys
+import Adafruit_DHT
+import time
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    time.sleep(1)
